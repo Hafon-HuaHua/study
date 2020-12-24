0a8c64d5c47020728adfe6c92489c0d974e1ef9d
hanfeng.sheng <hanfeng.sheng@ucarinc.com>
2020-12-24 14:48:32 +0800

    单例模式

--- base/src/main/java/com/study/base/pattern/singleton/Singleton_2.java
+++ base/src/main/java/com/study/base/pattern/singleton/Singleton_2.java
@@ -5,16 +5,16 @@ package com.study.base.pattern.singleton;
  * 工作中用的最多
  * 优点：加载类时不需要实例化
  */
-public class Singleton2 {
-    private static volatile Singleton2 singleton2;
+public class Singleton_2 {
+    private static volatile Singleton_2 singleton2;
     private static Object object = new Object();
-    private Singleton2(){}
+    private Singleton_2(){}
 
-    public static Singleton2 getInstance(){
+    public static Singleton_2 getInstance(){
         if(singleton2 == null){
             synchronized (object){
                 if(singleton2 == null){
-                    singleton2 = new Singleton2();
+                    singleton2 = new Singleton_2();
                 }
             }
         }
--- base/src/main/java/com/study/base/pattern/singleton/Singleton_4.java
+++ base/src/main/java/com/study/base/pattern/singleton/Singleton_4.java
@@ -3,7 +3,7 @@ package com.study.base.pattern.singleton;
 /**
  * 枚举单例
  * 最完美的一种方式，但工作中基本不用
- * 优点：保证线程安全，并且由于枚举的特性，保证单例，防止反序列化
+ * 优点：保证线程安全，并且由于枚举的特性，保证单例，防止反序列化（无法通过反射实例化，因为没有构造方法）
  */
 public enum Singleton_4 {
     INSTANCE;
