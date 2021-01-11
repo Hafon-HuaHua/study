ff2bd9603b7bbaff9d977bb99c9d906f471f79ac
hanfeng.sheng <hanfeng.sheng@ucarinc.com>
2021-01-11 17:27:48 +0800

    java stream案例

---
+++ base/src/main/java/com/study/base/stream/StreamTest.java
@@ -0,0 +1,160 @@
+package com.study.base.stream;
+
+import org.apache.commons.io.Charsets;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTest {
+
+    private static void write() {
+        try(OutputStream outputStream = new FileOutputStream("stream.txt")){
+            String writeText = "test hafon writeStream 哈哈哈";
+            byte[] bytes = writeText.getBytes(Charsets.UTF_8);
+            outputStream.write(bytes);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    private static void write_1(){
+        try(OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("stream.txt"))){
+            String writeText = "test hafon writeStream 嘿嘿嘿";
+            byte[] bytes = writeText.getBytes(Charsets.UTF_8);
+            outputStream.write(bytes);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+    private static void write_2(){
+        try(Writer writer = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream("stream.txt")),Charsets.UTF_8)){
+            String writeText = "test hafon writeStream 啦啦啦";
+            writer.write(writeText);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    private static void read(){
+        try(InputStream in = new FileInputStream("stream.txt")){
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] bytes = new byte[1024];
+            int readCount;
+            while((readCount = in.read(bytes)) != -1){
+                outputStream.write(bytes,0,readCount);
+            }
+            System.out.println(outputStream.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private static void read_1(){
+        try(InputStream in = new BufferedInputStream(new FileInputStream("stream.txt"))){
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] bytes = new byte[1024];
+            int readCount;
+            while((readCount = in.read(bytes)) != -1){
+                outputStream.write(bytes,0,readCount);
+            }
+            String str = outputStream.toString(Charsets.UTF_8.name());
+            System.out.println(str);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private static void read_2(){
+        try(Reader reader = new InputStreamReader(new BufferedInputStream(new FileInputStream("stream.txt")))){
+            char[] bytes = new char[1024];
+            reader.read(bytes);
+            System.out.println(new String(bytes));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private static void read_3(){
+        try(Reader reader = new InputStreamReader(new BufferedInputStream(new FileInputStream("stream.txt")))){
+            CharArrayWriter writer = new CharArrayWriter();
+            char[] chars = new char[1024];
+            int count;
+            while((count = reader.read(chars)) != -1){
+                writer.write(chars,0,count);
+            }
+            System.out.println(writer.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private static void datOutPut(){
+        List<Hafon> list = Arrays.asList(new Hafon(1, "h1"), new Hafon(2, "h2"));
+        try(DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("stream.dat")))){
+            out.writeInt(list.size());
+            for(Hafon h : list){
+                out.writeInt(h.getAge());
+                out.writeUTF(h.getName());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private static List<Hafon> dataInput(){
+        try(DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream("stream.dat")))){
+            int size = in.readInt();
+            List<Hafon> list = new ArrayList<>(size);
+            for(int i = 0;i < size;i++){
+                Hafon hafon = new Hafon();
+                hafon.setAge(in.readInt());
+                hafon.setName(in.readUTF());
+                list.add(hafon);
+            }
+            return list;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+     static class Hafon{
+        private int age;
+        private String name;
+        private Hafon(){}
+        private Hafon(int age,String name){
+            this.age = age;
+            this.name = name;
+        }
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+         @Override
+         public String toString() {
+             return "Hafon{" +
+                     "age=" + age +
+                     ", name='" + name + '\'' +
+                     '}';
+         }
+     }
+    public static void main(String[] args) {
+        /*List<Hafon> list = dataInput();
+        for(Hafon h : list){
+            System.out.println(h);
+        }*/
+        read_3();
+    }
+}
